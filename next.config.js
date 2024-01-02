bf81741403682d58ee706de4dcbf59f4cbd0ff48
amit <amitjanra579@gmail.com>
2024-01-02 13:43:46 +0530

    Update portfolio metadata and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//     ],
+//   },
+//   experimental: {
+//     serverActions: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   // output: 'export',
-  // distDir: "out",
   images: {
     remotePatterns: [
       {
@@ -10,9 +29,18 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: true,
-  },
-};
-
-module.exports = nextConfig;
+    experimental: {
+      serverActions: true,
+    },
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
